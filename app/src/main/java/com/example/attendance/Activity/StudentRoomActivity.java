d81e5f28ec47bf1330c0710541b69f60e582ebf0
Ayman Sabry <aymansabryh@gmail.com>
2021-05-17 16:38:25 +0200

    fff
    ffff

--- app/src/main/java/com/example/attendance/Activity/StudentRoomActivity.java
+++ app/src/main/java/com/example/attendance/Activity/StudentRoomActivity.java
@@ -1,17 +1,26 @@
 package com.example.attendance.Activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.example.attendance.Adapter.AdapterForRooms;
 import com.example.attendance.Domin.Room;
 import com.example.attendance.R;
 import com.example.attendance.RecyclerViewOnClickListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -31,6 +40,7 @@ public class StudentRoomActivity extends AppCompatActivity {
     private FirebaseUser firebaseUser;
     private ArrayList<Room> rooms;
     private AdapterForRooms adapterForAdminRooms;
+    private FloatingActionButton bnJoinRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +58,7 @@ public class StudentRoomActivity extends AppCompatActivity {
 
 
         recyclerView=findViewById(R.id.recycler_view_in_student_room);
+        bnJoinRoom =findViewById(R.id.floatingActionButtonJoinRoom);
 
 
 
@@ -96,8 +107,65 @@ public class StudentRoomActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_in_rooms_activity,menu);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_in_rooms).getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+        return true;
+
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
+
+
+       ViewGroup dialogView = (ViewGroup) LayoutInflater.from(getBaseContext()).inflate(R.layout.custom_dialog, null, false);
+        EditText etRoomTitle = dialogView.findViewById(R.id.editTextRoomTitle);
+
+
+
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this).setTitle("Join Room")
+                .setIcon(R.drawable.ic_alert_blue_24).setView(dialogView);
+        bnJoinRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.setPositiveButton("Add", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        String studentUid=firebaseUser.getUid();
+                        String roomId=etRoomTitle.getText().toString();
+
+databaseReference=FirebaseDatabase.getInstance().getReference("Student").child(studentUid).child(roomId);
+
+
+                    }
+                }).setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                try {
+                    alertDialog.show();
+                } catch (Exception e) {
+                    System.out.println("aaaaaaaaaaaaaaaaa   " + e.getMessage());
+                }
+
+            }
+        });
 
 
 
